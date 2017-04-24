1b84f789c989875559686a614a4b8ef2e64ddb5e
leohentschker <leohentschker@college.harvard.edu>
2017-04-24 16:26:25 -0400

    Fixing merge

--- test.ml
+++ test.ml
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-open Sexplib
-open Nocrypto
-
-let () = Nocrypto_entropy_unix.initialize ()
-
-let private_key_unsex = Dsa.generate (`Fips1024) ;;
-let pub = Dsa.pub_of_priv private_key_unsex ;;
-
-let digest = Cstruct.of_string "asd" ;;
-let signed = Dsa.sign ~key:private_key_unsex digest ;;
-
-let string_version = Sexp.to_string (Dsa.sexp_of_priv private_key_unsex) ;;
-let private_key = Sexp.of_string string_version ;;
-
-let verified = Dsa.verify ~key:pub signed digest in
-if verified then print_endline "FUCK YEAH VERIFIED" else print_endline "SHIT DIDN't WORK" ;;
-=======
-open Nocrypto
-
-
 let hash_text s =
     let init = Hash.SHA256.init () in
     Hash.SHA256.feed init (Cstruct.of_string s);
@@ -42,4 +21,3 @@ let mine s =
 
 let nonce = mine "ASD" ;;
 print_endline ("WORKING NONCE: " ^ nonce) ;;
->>>>>>> mining
