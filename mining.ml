6d852cf8469605d09b266d039fc4e3b2a104aa1e
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-04-24 21:16:53 -0400

    Exposed hash_text,fixed mining. Finds 3 zero has for ASD

--- mining.ml
+++ mining.ml
@@ -11,27 +11,27 @@ module Mining =
   (* Checks whether or not the hash of str with nonce on the back
      has *size* or more zeroes on the front *)
   let verify (size : int) (str : string) (nonce : int) : bool =
-    let combo = s ^ (string_of_int nonce) in
+    let combo = str ^ (string_of_int nonce) in
     let hashed = hash_text combo in
-    let first_chars = Str.first_chars hashed size in
-    if first_chars = String.make n "0"
+    let first_chars = String.sub hashed 0 size in
+    if first_chars = String.make size '0'
     then true 
     else false
   (* Implementation of the mining algorithm for proof-of-work *)
   let mine (s : string) (iters: int) : string =
-  (* Beginning nonce that is check*)
+  (* Beginning nonce *)
     let nonce = ref 0 in
-    let leading_zero_size = 10 in
+    let leading_zero_size = 3 in
     (* This inner function takes an integer and checks
        the has verify of s^nonce for nonce = 1,..., n *)
     let rec iterate_check n =
       if n = 0 then failwith "COULDN'T FIND" else
       nonce := !nonce + 1;
       if verify leading_zero_size s !nonce then
-        combo
+        s ^ (string_of_int !nonce)
       else iterate_check (n - 1) in
     iterate_check iters
   end 
 
-let nonce = mine "ASD" ;;
-print_endline ("WORKING NONCE: " ^ nonce) ;;
+  let nonce = Mining.mine "ASD" 20000000 ;;
+  print_endline ("WORKING NONCE: " ^ nonce ^ "Hash: " ^ Mining.hash_text nonce) ;;
--- mining.mli
+++ mining.mli
@@ -1,5 +1,6 @@
 module Mining :
   sig
+  	val hash_text : string -> string
     val verify : int -> string -> int -> bool
     val mine : string -> int -> string
   end
