1ecc5cae5eb02b7f8d37c48ae5759c1f7ef03cd0
leohentschker <leohentschker@college.harvard.edu>
2017-05-01 14:10:37 -0400

    Removing self ping

--- ocamlcoin.ml
+++ ocamlcoin.ml
@@ -7,16 +7,20 @@ module OcamlcoinRunner =
   struct
     (* store the other people in our network *)
     let peers : ocamlcoin_node list ref = ref []
+    (* only accept certain peers as being valid *)
+    let valid_peer ocamlcoin_node =
+      ocamlcoin_node#ip <> "127.0.0.1"
     (* load the peers we are aware of *)
     let load_peers ?(peer_file : string = "peers.txt") () =
-      peers := List.map
+      let unfiltered_peers = List.map
         (fun peer_description ->
           match Str.split (Str.regexp ",") peer_description with
           | [ip; port] ->
               new ocamlcoin_node ip (int_of_string port)
           | _ ->
               raise (Invalid_argument "Unable to parse peer description"))
-        (IO.page_lines peer_file)
+        (IO.page_lines peer_file) in
+        peers := List.filter valid_peer unfiltered_peers
     let broadcast_event event node =
       OcamlcoinNetwork.broadcast_to_node (event_to_json event) node
     let run () =
