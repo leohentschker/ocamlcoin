ca4cbf1798d2f408632d6063e46db311776e1e77
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-05-02 15:04:04 -0400

    ledgerrr

--- ledger.ml
+++ ledger.ml
@@ -4,25 +4,29 @@ open Crypto.Keychain
 open Payments
 open Merkletree
 open Wallet
+(* module Ledger =
+  struct *)
+  module MT = MakeMerkle (TransactionSerializable) (SHA256)
+  module W = MakeWallet
 
-module MT = MakeMerkle (TransactionSerializable) (SHA256)
-module W = MakeWallet
-
-let verify_transaction (t : transaction) (w : W.dict ref) : bool =
-  let id1, id2, amount = t#originator, t#target, t#amount in
-  match W.find !w id1 with
-  | None -> false
-  | Some b -> not (W.find !w id2 = None) && b < amount
-
-let add_transaction (t : transaction) (w : W.dict ref) (m : MT.mtree ref) : unit =
-  if verify_transaction t w then
-    m := (MT.add_element t !m);
+  let verify_transaction (t : transaction) (w : W.dict ref) : bool =
     let id1, id2, amount = t#originator, t#target, t#amount in
-    let currb1, currb2 = W.find id1, W.find id2 in
-    w := (W.insert (W.insert !w id1 (currb1 -. amount)) id2 (currb2 +. amount))
+    match W.find !w id1 with
+    | None -> false
+    | Some b -> not (W.find !w id2 = None) && b < amount
 
-let merge_trees (t1 : MT.mtree ref) (t2 : MT.mtree ref) : unit =
-    t1 := !t2
+  let add_transaction (t : transaction) (w : W.dict ref) (m : MT.mtree ref) : unit =
+    if verify_transaction t w then
+      m := (MT.add_element t !m);
+      let id1, id2, amount = t#originator, t#target, t#amount in
+      match W.find !w id1, W.find !w id2 with
+      | (Some currb1), (Some currb2) ->
+          w := (W.insert (W.insert !w id1 (currb1 -. amount)) id2 (currb2 +. amount))
+      | _, _ -> ()
 
-let query (s : string) (m : MT.mtree ref) : transaction list =
-  (queryid (string_to_pub s) !m) @ (queryhash s !m)
+  let merge_trees (t1 : MT.mtree ref) (t2 : MT.mtree ref) : unit =
+      t1 := !t2
+
+  let query (s : string) (m : MT.mtree ref) : transaction list =
+    (MT.queryid (string_to_pub s) !m) @ (MT.queryhash s !m)
+ (* end *)
