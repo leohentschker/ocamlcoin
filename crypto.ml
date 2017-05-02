5ba23c5cf6e765581d029e10231140bc1d01ff43
William Chang <William@Williams-MacBook-Air-3.local>
2017-05-02 16:06:16 -0400

    Tests

--- crypto.ml
+++ crypto.ml
@@ -20,22 +20,13 @@ open Keychain
 
 module Signature =
   struct
-    type signature = Cstruct.t * Cstruct.t
-    type priv_key = Dsa.priv
-    type pub_key = Dsa.pub
-    let priv_to_string (p : priv_key) = Sexp.to_string (Dsa.sexp_of_priv p)
-    let string_to_priv (s : string) = Dsa.priv_of_sexp (Sexp.of_string s)
-    let pub_to_string (p : pub_key) = Sexp.to_string (Dsa.sexp_of_pub p)
-    let string_to_pub (s : string) = Dsa.pub_of_sexp (Sexp.of_string s)
-    let generate_keypair () =
-      let private_key = Dsa.generate (`Fips1024) in
-      private_key, Dsa.pub_of_priv private_key
+    type signature = Cstruct.t * Cstruct.t  
     let sign (pk : priv_key) = Dsa.sign ~key:pk
     let verify (plaintext : string) (pub : pub_key) (s : signature) =
       Dsa.verify ~key:pub s (Cstruct.of_string plaintext)
 
     (* Testing in module because verify and sign are abstracted away *)
-    let test_generate_keypair () =
+    let test_generate_keypair () =i
       let (priv_key, pub_key) = generate_keypair () in
       assert (pub_key = pub_priv priv_key)
 
