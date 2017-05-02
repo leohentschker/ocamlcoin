5aa3249da398c21135c5f1e4bc8697f9854caf8e
Daniel Qu <dqu@college.harvard.edu>
2017-05-02 18:52:31 -0400

    semantics verify

--- bank.ml
+++ bank.ml
@@ -25,15 +25,15 @@ let add_transaction (t : transaction) (l : MT.mtree ref) : unit =
   if verify_transaction t l then
     ledger := (MT.add_element t !ledger)
 
-let rec verify_tree (t : MT.mtree) (n : int) : bool =
+let rec verify (t : MT.mtree) (n : int) : bool =
   if n = 0 then true
   else
     let tlist = MT.children t in
     let slist = sublist tlist 0 (n - 1) in
     let tn = List.nth tlist n in
-    verify_transaction tn (ref (build_tree slist)) && (verify_tree t (n - 1))
+    verify_transaction tn (ref (MT.build_tree slist)) && (verify t (n - 1))
 
-let verify (t : MT.mtree) = verify_tree t (List.length (children t) - 1)
+let verify_tree (t : MT.mtree) = verify t (List.length (MT.children t) - 1)
 
 let query (s : string) (m : MT.mtree ref) : transaction list =
   (MT.queryid (string_to_pub s) !m) @ (MT.queryhash s !m)
