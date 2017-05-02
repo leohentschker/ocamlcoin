ca62fbb18d45cc5b3301256df23dbecfc4b3e221
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-05-02 18:24:19 -0400

    fixed verified trans

--- bank.ml
+++ bank.ml
@@ -9,14 +9,16 @@ module MT = MakeMerkle (TransactionSerializable) (SHA256)
 
 let ledger = ref MT.empty
 
-let verify_transaction (t : transaction) : bool =
+let verify_transaction (t : transaction) (l: MT.mtree ref): bool =
   let id1, id2, amount, timestamp = t#originator, t#target, t#amount, t#timestamp in
-  let eltlst = MT.queryid id1 !ledger in 
+  let eltlst = MT.queryid id1 !l in 
   let timedlst = List.filter (fun x -> x#timestamp < timestamp) eltlst in 
-  let total_amount = List.fold_left (fun acc x -> acc +. x#amount) 0. timedlst in
+  let total_amount = List.fold_left (fun acc x -> if x#originator = id1 then acc - x#amount 
+                                     else acc + x#amount) 0. timedlst 
+  in
   not (eltlst = []) && (total_amount < amount) && 
-  (if amount < 0. then not (MT.queryid id2 !ledger = []) else true) &&
-  (Crypto.Signature.verify t#to_string t#pub_key t#signature)
+  (if amount < 0. then not (MT.queryid id2 !l = []) else true) &&
+  (Crypto.Signature.verify t#to_string t#originator t#signature)
 
 let add_transaction (t : transaction) : unit =
   if verify_transaction t then
