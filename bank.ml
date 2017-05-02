8e7eb44d26794d59be6e292085d1f37747f05e5d
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-05-02 17:00:43 -0400

    fixing bankl

--- bank.ml
+++ bank.ml
@@ -3,27 +3,22 @@ open Crypto
 open Crypto.Keychain
 open Payments
 open Merkletree
+(* let master = generate_keypair *)
 module MT = MakeMerkle (TransactionSerializable) (SHA256)
-module W = Wallet.MakeWallet
 
 let ledger = ref MT.empty
-let wallet = ref W.empty
 
 let verify_transaction (t : transaction) : bool =
-  let id1, id2, amount = t#originator, t#target, t#amount in
-  match W.find !wallet id1 with
-  | None -> false
-  | Some b -> not (W.find !wallet id2 = None) && b < amount
+  let id1, id2, amount, timestamp = t#originator, t#target, t#amount, t#timestamp in
+  let eltlst = MT.queryid id1 !ledger in 
+  let timedlst = List.filter (fun x -> x#timestamp < timestamp) eltlst in 
+  let total_amount = List.fold_left (fun x acc -> x#amount +. acc) 0. timedlst in
+  not (eltlst = 0) && (total_amount < amount) && 
+  (if amount < 0 then not (MT.queryid id2 ledger = []) else true)
 
 let add_transaction (t : transaction) : unit =
   if verify_transaction t then
-    ledger := (MT.add_element t !ledger);
-    let id1, id2, amount = t#originator, t#target, t#amount in
-    match W.find !wallet id1, W.find !wallet id2 with
-    | (Some currb1), (Some currb2) ->
-        wallet :=
-          (W.insert (W.insert !wallet id1 (currb1 -. amount)) id2 (currb2 +. amount))
-    | _, _ -> ()
+    ledger := (MT.add_element t !ledger)
 
 let merge_trees (t1 : MT.mtree ref)
                 (t2 : MT.mtree ref) : unit =
