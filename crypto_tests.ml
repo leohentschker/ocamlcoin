84e69f9ab010a85c78e38a0d507742f803bb5c08
leohentschker <leohentschker@college.harvard.edu>
2017-05-02 17:27:23 -0400

    Adding in fixes for payment

--- crypto_tests.ml
+++ crypto_tests.ml
@@ -1,14 +1,27 @@
+let test_key_serialize () =
+  let priv, pub = generate_keypair () in
+  let serialized_priv = string_to_priv (priv_to_string priv) in
+  let serialized_pub = string_to_pub (pub_to_string pub) in
+  assert(priv = serialized_priv);
+  assert(pub = serialized_pub)
+
 (* Testing in module because verify and sign are abstracted away *)
 let test_generate_keypair () =
   let (priv_key, pub_key) = generate_keypair () in
   assert (pub_key = Dsa.pub_of_priv priv_key)
 
+let test_signature_serialize () =
+  let (priv, pub) = generate_keypair () in
+  let signed = sign priv (TestHelpers.random_string ()) in
+  assert(signed = json_to_signature (signature_to_json signed))
+
 let test_sign () =
   let (priv_key, pub_key) = generate_keypair () in
-  let message = string_of_int (Random.int 10000000) in
+  let message = string_of_int (TestHelpers.random_string ()) in
   let signed = sign priv_key message in
   assert (verify message pub_key signed)
 
 let run_tests () =
+  TestHelpers.run_tests test_key_serialize;
   TestHelpers.run_tests test_generate_keypair;
   TestHelpers.run_tests test_sign
--- testHelpers.ml
+++ testHelpers.ml
@@ -1,5 +1,8 @@
 let c_TEST_ITERATIONS = 10 
 
+let random_string () =
+  string_of_int (Random.int 10000)
+
 let run_tests (test_func : unit -> unit) : unit =
   for _ = 1 to c_TEST_ITERATIONS do
     test_func ()
