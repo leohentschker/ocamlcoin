d559724acb606d5892211dc45a7a02dca98acae0
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-05-02 18:46:48 -0400

    Fixed bank

--- bank.ml
+++ bank.ml
@@ -13,15 +13,15 @@ let verify_transaction (t : transaction) (l: MT.mtree ref): bool =
   let id1, id2, amount, timestamp = t#originator, t#target, t#amount, t#timestamp in
   let eltlst = MT.queryid id1 !l in 
   let timedlst = List.filter (fun x -> x#timestamp < timestamp) eltlst in 
-  let total_amount = List.fold_left (fun acc x -> if x#originator = id1 then acc - x#amount 
-                                     else acc + x#amount) 0. timedlst 
+  let total_amount = List.fold_left (fun acc x -> if x#originator = id1 then acc -. x#amount 
+                                     else acc +. x#amount) 0. timedlst 
   in
   not (eltlst = []) && (total_amount < amount) && 
   (if amount < 0. then not (MT.queryid id2 !l = []) else true) &&
   authenticate_transaction t
 
-let add_transaction (t : transaction) : unit =
-  if verify_transaction t then
+let add_transaction (t : transaction) (l : MT.mtree ref) : unit =
+  if verify_transaction t l then
     ledger := (MT.add_element t !ledger)
 
 let merge_trees (t1 : MT.mtree ref)
