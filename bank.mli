2a942c041e8b580b8e96ef04af9f71c905c441a8
Vaughan McDonald <vaughanmcdonald@Vaughans-MacBook-Pro.local>
2017-05-02 22:21:57 -0400

    ..

--- bank.mli
+++ bank.mli
@@ -1,13 +1,13 @@
-(* open Crypto.Keychain
+open Crypto.Keychain
 open Crypto.Signature
+open Merkletree
 
-module Bank :
-  sig
+
+	module MT : Merkletree
     val masterkey : pub_key
-    val ledger : mtree ref
+    val ledger : MT.mtree ref
     val query : string -> MT.mtree ref -> transaction list
     val add_transaction : translation -> Merkletree.mtree ref -> unit
     val verify_transaction : transaction -> Merkletree.mtree ref -> bool
     val verify_ledger : Merkletree.mtree -> bool
     val merge_ledgers : Merkletree.mtree ref -> Merkletree.mtree ref -> unit
-  end *)
