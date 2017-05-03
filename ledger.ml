4bda27551b14e532378c588e160b4794cb22cdfc
Daniel Qu <dqu@college.harvard.edu>
2017-05-03 10:51:35 -0400

    added balance functions

--- ledger.ml
+++ ledger.ml
@@ -51,13 +51,18 @@ module Bank =
     let query (p : pub_key) (m : ledger) : transaction list =
       (MT.queryid p !m) @ (MT.queryhash (pub_to_string p) !m)
 
+    let get_balance (p : pub_key) (t : timestamp) (l : ledger) : float =
+      let eltlst = MT.queryid p !l in
+      let timedlst = List.filter (fun x -> x#timestamp < t) eltlst in
+      List.fold_left (fun acc x -> if x#originator = id then acc -. x#amount
+                                   else acc +. x#amount) 0. timedlst
+
+
     let verify_transaction (t : transaction) (l: ledger) : bool =
       let id1, id2, amount, timestamp = t#originator, t#target, t#amount, t#timestamp in
       let eltlst = MT.queryid id1 !l in
       let timedlst = List.filter (fun x -> x#timestamp < timestamp) eltlst in
-      let total_amount = List.fold_left
-        (fun acc x -> if x#originator = id1 then acc -. x#amount
-                      else acc +. x#amount) 0. timedlst in
+      let total_amount = get_balance id1 timestamp l in
       authenticate_transaction t
       && ((not (eltlst = [])
            && total_amount >= amount
